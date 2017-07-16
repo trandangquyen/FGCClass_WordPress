a7c5db13604aef37cbbde24508cf40e0b4616424
trandangquyen <trandangquyen@gmail.com>
2017-07-16 17:36:22 +0700

    add comments to fgc-teacher plugin

--- wp-content/plugins/fgc_teacher/fgc_teacher.php
+++ wp-content/plugins/fgc_teacher/fgc_teacher.php
@@ -12,12 +12,19 @@ define('FGC_ENDIR_URL',plugin_dir_url(__FILE__) );
 class FGC_Manager{
     function __construct()
     {
+        //add js vs css to admin panel
         add_action('admin_enqueue_scripts', array( $this, 'admin_style'));
+        //Create post type & taxonomy events
         add_action( 'init', array( $this, 'codex_events_init'));
+        // Create post type taxonomy news
         add_action( 'init', array( $this, 'codex_news_init'));
+        //Add metabox to postype events
         add_action('add_meta_boxes', array( $this, 'add_metabox_events'));
+        //Update news post when user click on the button 'Update' inside post edit area
         add_action('save_post', array($this,'save_selected_event'), 10, 3);
+        //Add column 'Sự kiện bài viết' to events postype
         add_filter( 'manage_events_posts_columns', array($this,'set_custom_edit_events_columns') );
+        //Add values of the column 'Sự kiện bài viết' to events postype
         add_action( 'manage_events_posts_custom_column' , array($this,'custom_events_column'), 10, 2 );
 
     }
@@ -162,21 +169,23 @@ class FGC_Manager{
     }
     // function to show events option
     function show_option_event($post,$metabox){
-        //get current post meta of this post
+        //Initialize the initial values
         $select_values = array('happening', 'upcoming', "expired");
-
-        $event_selected  = get_post_meta($post->ID,'event-post',true);
+        //start time value of the event
         $show_event_time_start  = get_post_meta($post->ID,'event-post-time-start',true);
+        //end time value of the event
         $show_event_time_end  = get_post_meta($post->ID,'event-post-time-end',true);
+        //location of the event
         $show_event_time_location  = get_post_meta($post->ID,'event-post-location',true);
-
-        $current_event_status = '';
+        //set time zone
         date_default_timezone_set('Asia/Ho_Chi_Minh');
+        //get current date
         $date = date('m/d/Y h:i A');
-        //echo $date.'===';
+        //convert current strings to time values
         $date = strtotime($date);
         $current_event_time_start = strtotime($show_event_time_start);
         $current_event_time_end = strtotime($show_event_time_end);
+        //set current event status
         if($date>=$current_event_time_start && $date<=$current_event_time_end) {
             $current_event_status = 'happening';
         }
@@ -192,7 +201,7 @@ class FGC_Manager{
             $current_event_status = 'expired';
         }
 
-
+        // begin print layout
         ?>
             <div class="date-time-picker">
                 <div class="row">
@@ -308,9 +317,6 @@ class FGC_Manager{
 
         }
     }
-
-
-
 
 }
 $fgc_manager = new FGC_Manager();
